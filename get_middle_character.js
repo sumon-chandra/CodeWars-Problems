6450688f61400fed612e5d71219a19ce27665dbf
Sumon Chandra Shil <topu199041@gmail.com>
2022-07-11 17:02:31 +0600

    Get the Middle Character

---
+++ get_middle_character.js
@@ -0,0 +1,16 @@
+const getMiddle = (s) => {
+  let position = 0;
+  let length = 0;
+  if (s.length % 2 === 1) {
+    position = s.length / 2;
+    length = 1;
+  } else {
+    position = s.length / 2 - 1;
+    length = 2;
+  }
+  return s.substring(position, position + length);
+};
+
+console.log(getMiddle("test")); // "es"
+console.log(getMiddle("middle")); // "dd"
+console.log(getMiddle("A")); // "A"
