6d652b9a42120da8083a4c8be6a6e7eaf95f1a90
Sumon Chandra Shil <topu199041@gmail.com>
2022-07-27 23:55:33 +0600

    One and Zero

---
+++ one_and_zero.js
@@ -0,0 +1,8 @@
+const binaryArrayToNumber = (arr) => {
+  const str = arr.join("");
+  return parseInt(str, 2);
+};
+console.log(binaryArrayToNumber([0, 0, 0, 1])); // 1
+console.log(binaryArrayToNumber([0, 0, 1, 0])); // 2
+console.log(binaryArrayToNumber([1, 1, 1, 1])); // 15
+console.log(binaryArrayToNumber([0, 1, 1, 0])); // 6
